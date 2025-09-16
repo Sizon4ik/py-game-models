611ea72f496ef66a7776ec5673aadb2fb3cf3172
Vitalii Mushta <vitaliimushta1993@gmail.com>
2025-09-16 13:53:14 +0200

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 import init_django_orm  # noqa: F401
-
+import json
 from db.models import Race, Skill, Player, Guild
+import datetime
 
 
 def main() -> None:
-    pass
+    with open('players.json', 'r') as f:
+        players = json.load(f)
+
+    for player, value in players.items():
+            nickname = player
+            email = value['email']
+            bio = value['bio']
+            race = value['race']
+            race_name = race.get('name')
+            race_description = race.get('description', None)
+            skills = race.get('skills')
+            guild = value['guild']
+
+            Race.objects.get_or_create(name=race_name, description=race_description)
+            race_id = Race.objects.get(name=race_name).id
+
+            if skills:
+                for skill in skills:
+                    Skill.objects.get_or_create(
+                        name=skill.get('name'), bonus=skill.get('bonus'),
+                        race_id=race_id
+                    )
+
+            if guild is not None:
+                guild_name = guild.get('name')
+                guild_description = guild.get('description', None)
+
+                Guild.objects.get_or_create(name=guild_name, description=guild_description)
+                guild_id = Guild.objects.get(name=guild_name).id
+
+                Player.objects.create(
+                    nickname=nickname, email=email, bio=bio,
+                                      race_id=race_id, guild_id=guild_id,
+                                      created_at=datetime.datetime.now()
+                )
+            else:
+                Player.objects.create(
+                    nickname=nickname, email=email, bio=bio,
+                    race_id=race_id, guild_id=None,
+                    created_at=datetime.datetime.now()
+                )
 
 
 if __name__ == "__main__":
